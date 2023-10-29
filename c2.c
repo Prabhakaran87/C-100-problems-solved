d21165d4c1c95b7d099754a6a12815ff584287e2
Prabhakaran87 <148926226+Prabhakaran87@users.noreply.github.com>
2023-10-29 15:53:18 +0530

    Add files via upload
    Taking input from user

---
+++ c2.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main()
+{
+    char c;
+    int a;
+    float f;
+
+    printf("Type the name\n");
+    scanf("%c", &c);
+    printf("Type the number\n");
+    scanf("%d", &a);
+    printf("Type the number\n");
+    scanf("%f", &f);
+    return 0;
+}
